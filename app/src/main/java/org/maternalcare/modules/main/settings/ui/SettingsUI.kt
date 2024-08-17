aed9e8e9b1707cfab58eefa9f02b8d390b3afad0
Miko Chu <miko.codecrafters@gmail.com>
2024-08-18 03:23:22 +0800

    add settings ui

---
+++ app/src/main/java/org/maternalcare/modules/main/settings/ui/SettingsUI.kt
@@ -0,0 +1,24 @@
+package org.maternalcare.modules.main.settings.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun SettingsUI(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {}) {
+            Text(text = "This is settings")
+        }
+    }
+}
