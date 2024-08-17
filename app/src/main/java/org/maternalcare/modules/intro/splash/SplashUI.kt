87b4374cc42b8f7d5e7940725cc47f0db1268a45
Reymond Pena <reymond.pena.dev@gmail.com>
2024-08-17 15:18:56 +0800

    modify splash ui

--- app/src/main/java/org/maternalcare/modules/intro/splash/SplashUI.kt
+++ app/src/main/java/org/maternalcare/modules/intro/splash/SplashUI.kt
@@ -1,14 +1,26 @@
 package org.maternalcare.modules.intro.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import org.maternalcare.R
 import org.maternalcare.modules.intro.IntroNav
 
 @Composable
@@ -18,10 +30,26 @@ fun SplashUI(navController: NavController) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = {
-            navController.navigate(IntroNav.Login)
-        }) {
-            Text(text = "Continue")
+        Image(painter = painterResource(id = R.drawable.care), contentDescription = "Login Image",
+            modifier = Modifier.size(200.dp))
+
+        Text(text = "Maternal Care", fontSize = 45.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = "Monitoring Application for Pregnant Women", fontSize = 12.sp)
+        Spacer(modifier = Modifier.height(70.dp))
+
+        Button(
+            onClick = { navController.navigate(IntroNav.Login)},
+            modifier = Modifier
+                .height(50.dp)
+                .width(140.dp),
+            shape = RoundedCornerShape(topStart = 10.dp, bottomEnd = 10.dp),
+            enabled = true,
+            elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 30.dp
+            )
+        ){
+            Text(text = "Get Started")
         }
     }
 }
