7d24bc7e6b4420fb2e23cb8a5118394c6e933fde
Miko Chu <miko.codecrafters@gmail.com>
2024-08-17 18:20:09 +0800

    declare flattened residence nav in main graph

--- app/src/main/java/org/maternalcare/modules/main/MainGraph.kt
+++ app/src/main/java/org/maternalcare/modules/main/MainGraph.kt
@@ -4,14 +4,32 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import org.maternalcare.modules.main.dashboard.ui.DashboardUI
 import org.maternalcare.modules.main.menu.MenuUI
-import org.maternalcare.modules.main.residence.residenceGraph
+import org.maternalcare.modules.main.residence.ui.AddressesUI
+import org.maternalcare.modules.main.residence.ui.CheckupDetailsUI
+import org.maternalcare.modules.main.residence.ui.ChooseCheckupUI
+import org.maternalcare.modules.main.residence.ui.ResidencesUI
 
 fun NavGraphBuilder.mainGraph(navController: NavController) {
     navigation<MainNav>(startDestination = MainNav.Menu) {
         composable<MainNav.Menu> {
             MenuUI(navController)
         }
-        residenceGraph(navController)
+        composable<MainNav.Addresses> {
+            AddressesUI(navController)
+        }
+        composable<MainNav.Residences> {
+            ResidencesUI(navController)
+        }
+        composable<MainNav.ChooseCheckup> {
+            ChooseCheckupUI(navController)
+        }
+        composable<MainNav.CheckupDetails> {
+            CheckupDetailsUI(navController)
+        }
+        composable<MainNav.Dashboard> {
+            DashboardUI(navController)
+        }
     }
 }
