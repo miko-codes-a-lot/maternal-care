eea8e2ed1ce6c9efa559d80c3638dd851f910264
Miko Chu <miko.codecrafters@gmail.com>
2024-08-18 03:30:43 +0800

    modify residence nav is archive option

--- app/src/main/java/org/maternalcare/modules/main/MainNav.kt
+++ app/src/main/java/org/maternalcare/modules/main/MainNav.kt
@@ -11,7 +11,7 @@ object MainNav {
     data class Addresses(val status: String)
 
     @Serializable
-    data class Residences(val status: String)
+    data class Residences(val status: String, var isArchive: Boolean = false)
 
     @Serializable
     object ChooseCheckup
