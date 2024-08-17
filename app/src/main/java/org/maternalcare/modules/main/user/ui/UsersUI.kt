9c2a9b61cf49182404b581d68079da74de33294a
Miko Chu <miko.codecrafters@gmail.com>
2024-08-18 03:23:27 +0800

    add users ui

---
+++ app/src/main/java/org/maternalcare/modules/main/user/ui/UsersUI.kt
@@ -0,0 +1,24 @@
+package org.maternalcare.modules.main.user.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun UsersUI(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {}) {
+            Text(text = "User1, User2, User3")
+        }
+    }
+}
