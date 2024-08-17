45f669e9aab1b01eeb6c985c379434ea53435bff
Miko Chu <miko.codecrafters@gmail.com>
2024-08-18 03:25:36 +0800

    optimize imports of main graph

--- app/src/main/java/org/maternalcare/modules/main/MainGraph.kt
+++ app/src/main/java/org/maternalcare/modules/main/MainGraph.kt
@@ -1,19 +1,11 @@
 package org.maternalcare.modules.main
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import org.maternalcare.modules.main.dashboard.ui.DashboardUI
 import org.maternalcare.modules.main.menu.MenuUI
-import org.maternalcare.modules.main.residence.enum.CheckupStatus
 import org.maternalcare.modules.main.residence.ui.AddressesUI
 import org.maternalcare.modules.main.residence.ui.CheckupDetailsUI
 import org.maternalcare.modules.main.residence.ui.ChooseCheckupUI
