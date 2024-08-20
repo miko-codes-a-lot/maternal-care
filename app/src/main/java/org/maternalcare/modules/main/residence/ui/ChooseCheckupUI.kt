7d4af1d70a1200fae5441542fcb8dec3df310abb
RPenaDev <159159701+ReymondDeveloper@users.noreply.github.com>
2024-08-21 00:09:20 +0800

    Feature - Residence - Choose Checkup Design (#8)
    * remove icon delete and edit
    
    * Modify residences checkup list selection UI
    
    * modify checkup details ui design with mock data
    
    * replaced old edit icon with new edit icon in floating action button
    
    * modify checkup details ui design with mock data
    
    * modify checkup details ui design and floating action button menu
    
    * modify choose checkup ui design
    
    * modify address ui design with percentage
    
    * modify dashboard navigation button
    
    * modify choose checkup ui design
    
    * deleted some additional spacer and box
    
    * modify addresses icon size and alignment

--- app/src/main/java/org/maternalcare/modules/main/residence/ui/ChooseCheckupUI.kt
+++ app/src/main/java/org/maternalcare/modules/main/residence/ui/ChooseCheckupUI.kt
@@ -1,27 +1,123 @@
 package org.maternalcare.modules.main.residence.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import org.maternalcare.R
 import org.maternalcare.modules.main.MainNav
+import org.maternalcare.modules.main.residence.enum.CheckupStatus
+
+//@Preview(showSystemUi = true)
+//@Composable
+//fun ChooseCheckupUIPrev() { ChooseCheckupUI(rememberNavController()) }
 
 @Composable
 fun ChooseCheckupUI(navController: NavController) {
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = {
+        Icon(
+            painter = painterResource(id = R.drawable.arrow),
+            contentDescription = "Exit Icon",
+            tint = Color.Black,
+            modifier = Modifier
+                .size(30.dp)
+                .offset(x = (-153).dp, y = (-95).dp)
+                .clickable {
+                    navController.navigate(MainNav.Residences(CheckupStatus.ALL.name))
+                }
+        )
+        Column(
+            modifier = Modifier
+            .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.care),
+                contentDescription = "Login Image",
+                modifier = Modifier.size(110.dp)
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "No. Of Check-up", fontSize = 23.sp,
+//                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.SansSerif
+            )
+            CheckUpNavigationButton(navController )
+
+        }
+    }
+}
+
+@Composable
+fun ButtonContainer(text : String, onClick:() -> Unit){
+    ElevatedButton(onClick = onClick,
+        colors = ButtonDefaults.elevatedButtonColors(
+            containerColor =  Color(0xFF6650a4),
+            contentColor = Color(0xFFFFFFFF)
+        ),
+        modifier = Modifier
+            .height(63.dp)
+            .width(280.dp)
+
+    ) {
+        Text(text = text, fontFamily = FontFamily.Serif,
+            fontSize = 20.sp
+        )
+    }
+}
+
+data class SelectionCheckUp (val text: String, val action: () -> Unit )
+
+@Composable
+fun CheckUpNavigationButton(navController: NavController){
+    val listOfCheckup = listOf(
+        SelectionCheckUp(text = "1st Checkup") {
             navController.navigate(MainNav.CheckupDetails)
-        }) {
-            Text(text = "1st checkup, 2nd checkup, 3rd checkup")
+        },
+        SelectionCheckUp(text = "2nd Checkup"){
+            navController.navigate(MainNav.CheckupDetails)
+        },
+        SelectionCheckUp(text = "3rd Checkup"){
+            navController.navigate(MainNav.CheckupDetails)
+        },
+        SelectionCheckUp(text = "4th Checkup"){
+            navController.navigate(MainNav.CheckupDetails)
+        }
+    )
+    Column (
+        modifier = Modifier.padding(20.dp)
+    ) {
+        listOfCheckup.forEach{ list ->
+            Spacer(modifier = Modifier.height(15.dp))
+            ButtonContainer(onClick = list.action, text = list.text )
         }
     }
 }
