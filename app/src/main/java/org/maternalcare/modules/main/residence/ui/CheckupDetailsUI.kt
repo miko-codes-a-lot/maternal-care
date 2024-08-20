6749d2ddb54b5dbf3042c3e8007f4202588eb745
Reymond Pena <reymond.pena.dev@gmail.com>
2024-08-20 23:53:00 +0800

    deleted some additional spacer and box

--- app/src/main/java/org/maternalcare/modules/main/residence/ui/CheckupDetailsUI.kt
+++ app/src/main/java/org/maternalcare/modules/main/residence/ui/CheckupDetailsUI.kt
@@ -9,21 +9,32 @@ import androidx.compose.animation.shrinkVertically
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -32,19 +43,11 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import org.maternalcare.R
 import org.maternalcare.modules.main.MainNav
-
-@Preview
-@Composable
-fun CheckupDetailsUIPreview () {
-    CheckupDetailsUI(rememberNavController())
-}
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -173,7 +176,7 @@ fun ParentFloatingIcon(navController: NavController) {
                                 painter = items[index],
                                 navController = navController
                             ) {
-                                navController.navigate(MainNav.ReviewCheckupStatusUI)
+                                navController.navigate(MainNav.ReviewCheckupStatus)
                             }
                             1 -> ChildIcon(
                                 painter = items[index],
@@ -216,15 +219,6 @@ fun ChildIcon(painter: Painter, navController: NavController, onClick: () -> Uni
             .fillMaxWidth()
             .padding(end = 5.dp)
     ) {
-        Spacer(modifier = Modifier.weight(1f))
-        Box(
-            modifier = Modifier
-                .background(Color(0xFF6650a4), RoundedCornerShape(10.dp))
-                .padding(11.dp)
-        )
-
-        Spacer(modifier = Modifier.width(10.dp))
-
         FloatingActionButton(
             onClick = onClick,
             modifier = Modifier.size(52.dp),
@@ -239,5 +233,3 @@ fun ChildIcon(painter: Painter, navController: NavController, onClick: () -> Uni
         }
     }
 }
-
-data class MiniFabItems(val icon: Painter, val title: String)
