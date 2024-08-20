6beb4082933f295d0b9dd73d6417035e69445c43
Miko Chu <miko.codecrafters@gmail.com>
2024-08-20 22:04:06 +0800

    add hilt config in main activity

--- app/src/main/java/org/maternalcare/MainActivity.kt
+++ app/src/main/java/org/maternalcare/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.FirebaseApp
+import dagger.hilt.android.AndroidEntryPoint
 import org.maternalcare.modules.intro.IntroNav
 import org.maternalcare.modules.intro.introGraph
 import org.maternalcare.modules.main.mainGraph
 import org.maternalcare.ui.theme.AppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
