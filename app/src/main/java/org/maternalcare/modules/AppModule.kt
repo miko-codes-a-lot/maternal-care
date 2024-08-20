d06e675ea345014668272cb1009d711569031605
Miko Chu <miko.codecrafters@gmail.com>
2024-08-20 23:26:01 +0800

    fix app module to provide firebase database

--- app/src/main/java/org/maternalcare/modules/AppModule.kt
+++ app/src/main/java/org/maternalcare/modules/AppModule.kt
@@ -2,12 +2,14 @@ package org.maternalcare.modules
 
 import com.google.firebase.database.FirebaseDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
     fun provideFirebaseDatabase(): FirebaseDatabase {
         return FirebaseDatabase.getInstance()
     }
