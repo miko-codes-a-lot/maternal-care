09d5ca929e4642669584314ba90adeb8a750e215
Miko Chu <miko.codecrafters@gmail.com>
2024-08-28 19:35:01 +0800

    remove firebase usage

--- app/src/main/java/org/maternalcare/modules/main/user/service/UserService.kt
+++ app/src/main/java/org/maternalcare/modules/main/user/service/UserService.kt
@@ -1,25 +1,21 @@
 package org.maternalcare.modules.main.user.service
 
-import com.google.firebase.database.FirebaseDatabase
-import kotlinx.coroutines.tasks.await
 import org.maternalcare.modules.main.user.model.UserDto
 import java.util.UUID
 import javax.inject.Inject
 
-class UserService @Inject constructor(fdb: FirebaseDatabase) {
-    private val userRefs = fdb.getReference("users")
-
+class UserService @Inject constructor() {
     suspend fun fetch(): List<UserDto> {
-        val snapshot = userRefs.get().await()
-        return snapshot.children.map { userSnapshot ->
-            userSnapshot.getValue(UserDto::class.java)!!
-        }
+        return emptyList()
     }
 
     suspend fun create(user: UserDto): Result<UserDto> {
         return try {
             user.id = UUID.randomUUID().toString()
-            userRefs.child(user.id).setValue(user).await()
+
+            if (user.id.isNotEmpty()) {
+                throw Exception("Testing")
+            }
 
             Result.success(user)
         } catch (exception: Exception) {
