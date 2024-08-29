b09b33eab87aeba92afb1be2441098a9c1a9c178
Miko Chu <miko.codecrafters@gmail.com>
2024-08-29 22:47:26 +0800

    add realm database provider

--- app/src/main/java/org/maternalcare/modules/AppModule.kt
+++ app/src/main/java/org/maternalcare/modules/AppModule.kt
@@ -4,12 +4,41 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.realm.kotlin.Realm
+import io.realm.kotlin.ext.query
+import io.realm.kotlin.mongodb.App
+import io.realm.kotlin.mongodb.Credentials
+import io.realm.kotlin.mongodb.sync.SyncConfiguration
+import kotlinx.coroutines.runBlocking
+import org.maternalcare.BuildConfig
+import org.maternalcare.modules.main.user.model.entity.UserEntity
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
     @Provides
-    fun provideAny(): Any {
-        return ""
+    @Singleton
+    fun provideRealmDatabase(): Realm {
+        return runBlocking {
+            val app = App.create(BuildConfig.REALM_APP_ID)
+            val credentials = Credentials.apiKey(BuildConfig.REALM_API_KEY)
+            val user = app.login(credentials)
+
+            val config = SyncConfiguration
+                .Builder(
+                    user,
+                    setOf(UserEntity::class)
+                )
+                .initialSubscriptions { realm ->
+                    add(
+                        realm.query<UserEntity>("_id <> $0", null),
+                        name = "Users"
+                    )
+                }
+                .build()
+
+            Realm.open(config)
+        }
     }
 }
