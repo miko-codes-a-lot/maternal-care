54f7e98c897fdbd652bee14610f2e898a968642d
RPenaDev <159159701+ReymondDeveloper@users.noreply.github.com>
2024-08-30 21:09:36 +0800

    Feature - Residence - Edit CheckUp - UI Design Edit (#24)
    * modify edit checkup ui design
    
    * remove the floating action icon and change the container from a box to a row. Additionally, add the NumberOfCheckUp function for the title in the CheckupDetailsUI

--- app/src/main/java/org/maternalcare/modules/main/residence/ui/EditCheckupUI.kt
+++ app/src/main/java/org/maternalcare/modules/main/residence/ui/EditCheckupUI.kt
@@ -1,25 +1,24 @@
 package org.maternalcare.modules.main.residence.ui
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,10 +27,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import org.maternalcare.modules.main.MainNav
 
 @Preview
 @Composable
@@ -41,111 +39,106 @@ fun EditCheckupUIPreview() {
 
 @Composable
 fun EditCheckupUI(navController: NavController) {
-    Dialog(
-        onDismissRequest = {},
-        properties = DialogProperties(
-            usePlatformDefaultWidth = false
-        )
-    ) {
-        Card(
-            elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
-            shape = RoundedCornerShape(15.dp),
-            modifier = Modifier
-                .fillMaxWidth(0.9f)
-                .height(480.dp)
-                .border(2.dp, color = Color.DarkGray, shape = RoundedCornerShape(15.dp))
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(15.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = "Checkup Details",
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = FontFamily.Serif
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-
-                ContainerValue()
-
-                Spacer(modifier = Modifier.height(13.dp))
-                Button(
-                    onClick = {
-                        navController.popBackStack()
-                    },
-                    modifier = Modifier
-                        .width(200.dp)
-                        .height(40.dp)
-                ) {
-                    Text("Submit")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun ContainerValue() {
     val initialValues = listOf(
-        "Height",
-        "Weight",
-        "Last Menstrual Period",
-        "Expected Due Date",
-        "Age Of Gestation",
-        "Nutritional Status",
-        "Schedule of Next Check-up"
+        "Blood Pressure","Height", "Weight", "Last Menstrual Period",
+        "Expected Due Date", "Age Of Gestation", "Nutritional Status",
+        "Next Check-up"
     )
+    val states = remember { initialValues.associateWith { mutableStateOf("") } }
 
-    val states = remember {
-        initialValues.associateWith { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ContainerValue(states)
+        Spacer(modifier = Modifier.height(8.dp))
+        ButtonSaveEdit(navController, states)
     }
+}
 
-    states.forEach { (label, state) ->
-        EditDetailsList(labelContainer = label, sampleValue = state.value) { newValue ->
-            state.value = newValue
+@Composable
+fun ContainerValue(statesValue: Map<String, MutableState<String>>) {
+    statesValue.forEach { (labels, states) ->
+        EditDetailsList(labelContainer = labels, sampleValue = states.value) { newValues ->
+            states.value = newValues
         }
     }
 }
 
 @Composable
-fun EditDetailsList(labelContainer: String, sampleValue: String, onValueChange: (String) -> Unit) {
-    var text by remember { mutableStateOf(sampleValue) }
+fun EditDetailsList(
+    labelContainer: String,
+    sampleValue: String,
+    onValueChange: (String) -> Unit
+) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(5.dp)
+            .padding(top = 8.dp)
     ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(9.dp)
-                .height(20.dp)
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = labelContainer,
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = FontFamily.SansSerif
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = labelContainer,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.SansSerif,
+                fontSize = 17.sp
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(" : ", fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.width(8.dp))
+            TextField(
+                value = sampleValue,
+                onValueChange = { newValue ->
+                    onValueChange(newValue)
+                },
+                placeholder = { Text("Enter value") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(51.dp),
+                isError = sampleValue.isNotEmpty() && !isValidText(sampleValue),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    errorContainerColor = Color.Transparent,
+                    errorIndicatorColor = Color.Red
                 )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(" : ", fontWeight = FontWeight.Bold)
-                Spacer(modifier = Modifier.width(8.dp))
-                TextField(
-                    value = text,
-                    onValueChange = {
-                        text = it
-                        onValueChange(it)
-                    },
-                    placeholder = { Text("Enter value") },
-                    modifier = Modifier.fillMaxWidth(),
-                )
-            }
+            )
         }
+    }
+}
+
+fun isValidText(inputValue: String): Boolean {
+    return inputValue.matches(Regex("[a-zA-Z]+"))
+}
+
+@Composable
+fun ButtonSaveEdit(
+    navController: NavController,
+    states: Map<String, MutableState<String>>
+){
+    Button(
+        onClick = {
+            val allFieldsValid = states.values.all { it.value.isNotBlank() }
+            if (allFieldsValid) {
+                navController.navigate(MainNav.CheckupDetails)
+            }
+        },
+        modifier = Modifier
+            .width(360.dp)
+            .padding(top = 27.dp)
+            .height(54.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFF6650a4),
+            contentColor = Color(0xFFFFFFFF)
+        )
+    ) {
+        Text(text = "Confirm", fontSize = 17.sp)
     }
 }
