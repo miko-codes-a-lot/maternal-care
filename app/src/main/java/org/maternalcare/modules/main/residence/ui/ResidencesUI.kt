6f068d586d21b25a690f43c313dbad6d418d05ab
Reymond Pena <reymond.pena.dev@gmail.com>
2024-08-31 00:09:13 +0800

    remove the residence icons and replace them with an image container in the Residences UI.

--- app/src/main/java/org/maternalcare/modules/main/residence/ui/ResidencesUI.kt
+++ app/src/main/java/org/maternalcare/modules/main/residence/ui/ResidencesUI.kt
@@ -1,5 +1,8 @@
 package org.maternalcare.modules.main.residence.ui
 
+import android.net.Uri
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,11 +15,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -29,15 +33,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
 import org.maternalcare.R
 import org.maternalcare.modules.main.MainNav
 import org.maternalcare.modules.main.residence.enum.CheckupStatus
+
+@Preview(showSystemUi = true)
+@Composable
+fun ResidencePreview() {
+    ResidencesUI(navController = rememberNavController())
+}
 
 @Composable
 fun ResidencesUI(navController: NavController) {
@@ -47,7 +62,9 @@ fun ResidencesUI(navController: NavController) {
             .padding(16.dp)
     ) {
         Column(
-            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -60,6 +77,40 @@ fun ResidencesUI(navController: NavController) {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun UsersImageContainer(imageUri: Uri? = null) {
+    Box(
+        Modifier.height(35.dp)
+    ){
+       Box(
+           modifier = Modifier
+               .size(35.dp)
+               .clip(CircleShape)
+               .background(Color(0xFF6650a4))
+               .border(3.dp, Color(0xFF6650a4), CircleShape),
+           contentAlignment = Alignment.Center
+       ) {
+           if (imageUri != null) {
+               AsyncImage(
+                   model = imageUri,
+                   contentDescription = null,
+                   contentScale = ContentScale.Crop,
+                   modifier = Modifier
+                       .size(35.dp)
+                       .clip(CircleShape)
+               )
+           } else {
+               Icon(
+                   painter = painterResource(id = R.drawable.person),
+                   contentDescription = "Default placeholder",
+                   modifier = Modifier.size(23.dp),
+                   tint = Color.White
+               )
+           }
+       }
     }
 }
 
@@ -84,27 +135,23 @@ fun SingleItemCard(residenceName: String,navController: NavController) {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(10.dp)
-                .clickable {navController.navigate(MainNav.ChooseCheckup)},
+                .clickable { navController.navigate(MainNav.ChooseCheckup) },
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Icon(
-                painter = painterResource(id = R.drawable.person),
-                contentDescription = "Person Icon",
-                tint = Color(0xFF6650a4),
-                modifier = Modifier.size(26.dp)
-            )
+            UsersImageContainer()
             Text(
                 text = residenceName,
                 fontSize = 18.sp,
                 fontFamily = FontFamily.SansSerif,
-                modifier = Modifier.padding(start = 8.dp)
+                modifier = Modifier
+                    .padding(start = 8.dp)
                     .weight(1f)
             )
         }
-        Divider(
-            color = Color(0xFF6650a4),
+        HorizontalDivider(
+            modifier = Modifier.fillMaxWidth(),
             thickness = 1.dp,
-            modifier = Modifier.fillMaxWidth()
+            color = Color(0xFF6650a4)
         )
     }
 }
