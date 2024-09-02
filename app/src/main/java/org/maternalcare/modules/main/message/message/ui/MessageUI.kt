063159d97af6bab0973c68ac0bca5bf9d6e28fe0
Reymond Pena <reymond.pena.dev@gmail.com>
2024-09-02 21:39:29 +0800

    modify message ui design and add icon send

--- app/src/main/java/org/maternalcare/modules/main/message/message/ui/MessageUI.kt
+++ app/src/main/java/org/maternalcare/modules/main/message/message/ui/MessageUI.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,23 +14,37 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import org.maternalcare.R
+
+data class SMSMessage(
+    val type: Int,
+    val message: String,
+    val date: String
+)
 
 @Preview(showSystemUi = true)
 @Composable
@@ -43,13 +56,11 @@ fun MessageUIPreview() {
 fun MessageUI(navController: NavController) {
     Column(
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxSize()
             .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
-        Text(text = "This is Message Container")
         MessageContainer()
     }
 }
@@ -57,58 +68,65 @@ fun MessageUI(navController: NavController) {
 @Composable
 fun MessageContainer() {
     val messages = listOf(
-        SMSMessage(type = 1, message = "Hello from User", date = "2024-09-01 10:00:00"),
-        SMSMessage(type = 2, message = "Hello from Client", date = "2024-09-01 10:05:00")
+        SMSMessage(type = 1, message = "Hello from User sdfsdfsdfsdfdsf", date = "2024-09-01 10:00:00"),
+        SMSMessage(type = 2, message = "Hello from Client", date = "2024-09-01 10:05:00"),
+        SMSMessage(type = 1, message = "Another message from User", date = "2024-09-01 10:10:00"),
+        SMSMessage(type = 2, message = "Another message from Client", date = "2024-09-01 10:15:00")
     )
-
-    LazyColumn(
-        modifier = Modifier
-            .border(1.dp, color = Color.Gray)
-            .height(380.dp)
-            .fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
+    val listState = rememberLazyListState()
+    Column(modifier = Modifier
+        .height(660.dp)
     ) {
-        items(messages) { message ->
-            SenderView(sender = if (message.type == 1) "User" else "Client")
-            MessageView(message = message)
+        LazyColumn(
+            state = listState,
+            modifier = Modifier
+                .background(Color.White)
+                .weight(1f)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            reverseLayout = true
+        ) {
+            items(messages) { message ->
+                MessageView(message = message)
+            }
         }
-
+        MessageInputField()
+    }
+    LaunchedEffect(messages.size) {
+        listState.animateScrollToItem(0)
     }
 }
 
-data class SMSMessage(
-    val type: Int,
-    val message: String,
-    val date: String
-)
-
 @Composable
 fun MessageView(message: SMSMessage) {
-    // State to keep track of whether the message is expanded
-    var isExpanded = remember { mutableStateOf(false) }
+    val (containerColor, contentColor) = if (message.type == 1) {
+        Color(0xFF6650a4) to Color(0xFFFFFFFF)
+    } else { Color(0xFFcccccf) to Color.Black}
+
+    val horizontalArrangement = if (message.type == 1) Arrangement.End else Arrangement.Start
 
     Row(
         modifier = Modifier
+            .padding(0.dp)
             .fillMaxWidth(),
-        horizontalArrangement = if (message.type == 1) Arrangement.Start else Arrangement.End
+        horizontalArrangement = horizontalArrangement
     ) {
         Surface(
             modifier = Modifier
                 .fillMaxWidth(0.7f)
-                .padding(10.dp)
-                .clickable { isExpanded.value = !isExpanded.value }, // Toggle expansion on click
+                .padding(top = 10.dp, bottom = 10.dp),
             shape = RoundedCornerShape(12.dp),
-            color = if (message.type == 1) MaterialTheme.colorScheme.primaryContainer else MaterialTheme.colorScheme.primary
+            color = containerColor,
+            contentColor = contentColor
         ) {
-            Column(modifier = Modifier.padding(12.dp)) {
-                Text(text = message.message)
-                if (isExpanded.value) {
-                    // Show full message when expanded
-                    Text(
-                        text = "Full message content: ${message.message}", // Example, replace with actual logic
-                        modifier = Modifier.padding(top = 8.dp)
-                    )
-                }
+            Column(modifier = Modifier
+                .padding(12.dp)
+            ) {
+                Text(
+                    text = message.message,
+                    maxLines = Int.MAX_VALUE,
+                    overflow = TextOverflow.Visible
+                )
                 Spacer(modifier = Modifier.height(8.dp))
                 Text(
                     text = message.date.split(" ").last(),
@@ -123,22 +141,41 @@ fun MessageView(message: SMSMessage) {
     }
 }
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SenderView(sender: String) {
-    Box(
+fun MessageInputField() {
+    val messageText = remember { mutableStateOf("") }
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .size(64.dp)
-            .background(MaterialTheme.colorScheme.surface),
-        contentAlignment = Alignment.Center
+            .background(Color.White, RoundedCornerShape(8.dp))
+            .border(1.dp, Color(0xFF6650a4), RoundedCornerShape(8.dp)),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(
-            text = sender,
-            fontSize = MaterialTheme.typography.titleLarge.fontSize,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.onSurface,
-            modifier = Modifier.padding(12.dp)
+        TextField(
+            value = messageText.value,
+            onValueChange = { messageText.value = it },
+            placeholder = { Text("Type your message...")},
+            modifier = Modifier
+                .weight(1f)
+                .padding(8.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedContainerColor = Color.White
+            )
         )
+        Icon(
+            painter = painterResource(id = R.drawable.icon_send),
+            contentDescription = "Send",
+            modifier = Modifier
+                .padding(8.dp)
+                .size(25.dp)
+                .clickable {
+                    println("Sending message: ${messageText.value}")
+                    messageText.value = ""
+                },
+            tint = Color(0xFF6650a4)
+        )
+        Spacer(modifier = Modifier.padding(end = 10.dp))
     }
 }
