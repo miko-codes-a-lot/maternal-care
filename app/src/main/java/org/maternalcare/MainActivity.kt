5ac76029e32d65d0241a0f600cae90480176aff1
Reymond Pena <reymond.pena.dev@gmail.com>
2024-09-10 01:00:18 +0800

    add loading icon in setting ui and implement userState, composition local provider in Main Activity, additionally add user state view model

--- app/src/main/java/org/maternalcare/MainActivity.kt
+++ app/src/main/java/org/maternalcare/MainActivity.kt
@@ -4,25 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.maternalcare.modules.intro.IntroNav
+import org.maternalcare.modules.intro.UserState
+import org.maternalcare.modules.intro.UserStateViewModel
 import org.maternalcare.modules.intro.introGraph
 import org.maternalcare.modules.main.mainGraph
 import org.maternalcare.ui.theme.AppTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val userState by viewModels<UserStateViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AppTheme {
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = IntroNav) {
-                    introGraph(navController)
-                    mainGraph(navController)
+                CompositionLocalProvider(UserState provides userState) {
+                    NavHost(navController = navController, startDestination = IntroNav) {
+                        introGraph(navController)
+                        mainGraph(navController)
+                    }
                 }
             }
         }
