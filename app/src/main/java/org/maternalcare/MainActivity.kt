ea72e8eb699b140cb13c0f39dc811b0a76389479
Reymond Pena <reymond.pena.dev@gmail.com>
2024-09-11 12:34:34 +0800

    modifying the condition and value in auth service and main activity

--- app/src/main/java/org/maternalcare/MainActivity.kt
+++ app/src/main/java/org/maternalcare/MainActivity.kt
@@ -6,13 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.maternalcare.modules.intro.IntroNav
 import org.maternalcare.modules.intro.introGraph
+import org.maternalcare.modules.intro.login.viewmodel.LoginViewModel
 import org.maternalcare.modules.intro.login.viewmodel.UserState
 import org.maternalcare.modules.intro.login.viewmodel.UserStateViewModel
+import org.maternalcare.modules.main.MainNav
 import org.maternalcare.modules.main.mainGraph
 import org.maternalcare.ui.theme.AppTheme
 
@@ -23,15 +26,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val loginViewModel: LoginViewModel = hiltViewModel()
+
             AppTheme {
                 val navController = rememberNavController()
                 CompositionLocalProvider(UserState provides userState) {
-                    NavHost(navController = navController, startDestination = IntroNav) {
+                    val startingDestination = getStartDestination(loginViewModel)
+                    NavHost(navController = navController, startDestination = startingDestination) {
                         introGraph(navController)
                         mainGraph(navController)
                     }
                 }
             }
         }
+    }
+
+    private fun getStartDestination(loginViewModel: LoginViewModel): Any {
+        if (loginViewModel.getLoggedInUserId() != null){
+            return MainNav
+        }
+        return IntroNav
     }
 }
