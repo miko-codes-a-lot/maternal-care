d4398d7ca226230141f8592b09a5a79f57da598c
Miko Chu <miko.codecrafters@gmail.com>
2024-09-16 08:06:20 +0800

    modify user mapper based on user & extension changes

--- app/src/main/java/org/maternalcare/modules/main/user/model/mapper/UserMapper.kt
+++ app/src/main/java/org/maternalcare/modules/main/user/model/mapper/UserMapper.kt
@@ -3,9 +3,9 @@ package org.maternalcare.modules.main.user.model.mapper
 import io.realm.kotlin.types.RealmInstant
 import org.maternalcare.modules.main.user.model.dto.UserDto
 import org.maternalcare.modules.main.user.model.entity.User
+import org.maternalcare.shared.ext.toInstantString
 import org.maternalcare.shared.ext.toObjectId
 import org.maternalcare.shared.ext.toRealmInstant
-import org.maternalcare.shared.util.DateUtil
 
 fun User.toDTO(): UserDto {
     return UserDto(
@@ -15,14 +15,14 @@ fun User.toDTO(): UserDto {
         lastName = lastName,
         email = email,
         mobileNumber = mobileNumber,
-        dateOfBirth = DateUtil.from(dateOfBirth),
+        dateOfBirth = dateOfBirth.toInstantString(),
         password = password,
         createdBy = createdBy?.toString(),
-        createdAt = DateUtil.from(createdAt),
+        createdAt = createdAt.toInstantString(),
         lastUpdatedBy = lastUpdatedBy?.toString(),
-        lastUpdatedAt = DateUtil.from(lastUpdatedAt),
+        lastUpdatedAt = lastUpdatedAt.toInstantString(),
         deletedBy = deletedBy?.toString(),
-        deletedAt = deletedAt?.run { DateUtil.from(this) },
+        deletedAt = deletedAt?.run { this.toInstantString() },
         isSuperAdmin = isSuperAdmin,
         isAdmin = isAdmin,
         isResidence = isResidence,
@@ -42,14 +42,14 @@ fun UserDto.toEntity(): User {
         lastName = userDto.lastName
         email = userDto.email
         mobileNumber = userDto.mobileNumber
-        dateOfBirth = DateUtil.parse(userDto.dateOfBirth)
+        dateOfBirth = userDto.dateOfBirth.toRealmInstant()
         password = userDto.password
         createdBy = userDto.createdBy?.toObjectId()
         createdAt = userDto.createdAt.toRealmInstant() ?: RealmInstant.now()
         lastUpdatedBy = userDto.lastUpdatedBy?.toObjectId()
         lastUpdatedAt = userDto.createdAt.toRealmInstant() ?: RealmInstant.now()
         deletedBy = userDto.deletedBy?.toObjectId()
-        deletedAt = userDto.deletedAt?.run { DateUtil.parse(this) }
+        deletedAt = userDto.deletedAt?.run { this.toRealmInstant() }
         isSuperAdmin = userDto.isSuperAdmin
         isAdmin = userDto.isAdmin
         isResidence = userDto.isResidence
