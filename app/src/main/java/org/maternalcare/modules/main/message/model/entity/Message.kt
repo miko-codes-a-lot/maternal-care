5628dede6c99141222f2cb628a19bce3e2f53569
Miko Chu <miko.codecrafters@gmail.com>
2024-09-16 08:05:22 +0800

    add message entity

---
+++ app/src/main/java/org/maternalcare/modules/main/message/model/entity/Message.kt
@@ -0,0 +1,17 @@
+package org.maternalcare.modules.main.message.model.entity
+
+import io.realm.kotlin.types.RealmInstant
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.ObjectId
+
+class Message: RealmObject {
+    @PrimaryKey
+    var _id: ObjectId = ObjectId()
+
+    var senderId: ObjectId? = null
+
+    var receiverId: ObjectId? = null
+
+    var date: RealmInstant = RealmInstant.now()
+}
