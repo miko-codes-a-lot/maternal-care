76cf667babcbf1feffb9a35ed8475cecac2c68ac
Miko Chu <miko.codecrafters@gmail.com>
2024-09-16 11:51:21 +0800

    modify message view model to get residences based on creator

--- app/src/main/java/org/maternalcare/modules/main/message/viewmodel/MessageViewModel.kt
+++ app/src/main/java/org/maternalcare/modules/main/message/viewmodel/MessageViewModel.kt
@@ -6,6 +6,7 @@ import org.maternalcare.modules.main.message.model.dto.MessageDto
 import org.maternalcare.modules.main.message.service.MessageService
 import org.maternalcare.modules.main.user.model.dto.UserDto
 import org.maternalcare.modules.main.user.service.UserService
+import org.maternalcare.shared.ext.toObjectId
 import org.mongodb.kbson.ObjectId
 import javax.inject.Inject
 
@@ -14,8 +15,8 @@ class MessageViewModel @Inject constructor(
     private val messageService: MessageService,
     private val userService: UserService
 ): ViewModel() {
-    fun getResidences(): List<UserDto> {
-        return userService.fetch(isResidence = true)
+    fun getResidences(userId: String): List<UserDto> {
+        return userService.fetch(isResidence = true, userId = userId.toObjectId())
     }
 
     fun fetchMessages(senderId: ObjectId, receiverId: ObjectId): List<MessageDto> {
