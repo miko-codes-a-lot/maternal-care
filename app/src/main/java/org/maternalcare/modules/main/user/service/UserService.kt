56ed020864690f143f21aebaffe3aac7e7bc73dc
Miko Chu <miko.codecrafters@gmail.com>
2024-09-16 11:48:29 +0800

    modify user service to optionally retrieve by created by user

--- app/src/main/java/org/maternalcare/modules/main/user/service/UserService.kt
+++ app/src/main/java/org/maternalcare/modules/main/user/service/UserService.kt
@@ -11,15 +11,22 @@ import org.mongodb.kbson.ObjectId
 import javax.inject.Inject
 
 class UserService @Inject constructor(private val realm: Realm) {
-    fun fetch(isResidence: Boolean = false): List<UserDto> {
-        return realm.query<User>("isResidence == $0", isResidence)
+    fun fetch(isResidence: Boolean = false, userId: ObjectId? = null): List<UserDto> {
+        val query = StringBuilder()
+            .append("isResidence == $0")
+
+        if (userId != null) query.append(" AND createdById == $1")
+
+        return realm.query<User>(query.toString(), isResidence, userId)
             .find()
             .map { user -> user.toDTO() }
     }
 
-    suspend fun upsert(data: UserDto): Result<UserDto> {
+    suspend fun upsert(data: UserDto, actionOf: UserDto): Result<UserDto> {
         return try {
             realm.write {
+                data.createdById = actionOf.id
+                data.lastUpdatedById = data.lastUpdatedById ?: actionOf.id
                 val user = copyToRealm(data.toEntity(), updatePolicy = UpdatePolicy.ALL)
                 Result.success(user.toDTO());
             }
