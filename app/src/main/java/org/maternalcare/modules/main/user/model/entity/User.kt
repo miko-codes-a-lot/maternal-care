c3b25195924d625804e1914b23b9f52f683e7e89
Miko Chu <miko.codecrafters@gmail.com>
2024-09-16 08:11:50 +0800

    refactor created, updated, deleted by to include id at the end

--- app/src/main/java/org/maternalcare/modules/main/user/model/entity/User.kt
+++ app/src/main/java/org/maternalcare/modules/main/user/model/entity/User.kt
@@ -15,11 +15,11 @@ class User : RealmObject {
     var mobileNumber: String? = ""
     var dateOfBirth: RealmInstant = RealmInstant.now()
     var password: String = ""
-    var createdBy: ObjectId? = null
+    var createdById: ObjectId? = null
     var createdAt: RealmInstant = RealmInstant.now()
-    var lastUpdatedBy: ObjectId? = null
+    var lastUpdatedById: ObjectId? = null
     var lastUpdatedAt: RealmInstant = RealmInstant.now()
-    var deletedBy: ObjectId? = null
+    var deletedById: ObjectId? = null
     var deletedAt: RealmInstant? = null
     var isActive: Boolean = true
     var isSuperAdmin: Boolean = false
