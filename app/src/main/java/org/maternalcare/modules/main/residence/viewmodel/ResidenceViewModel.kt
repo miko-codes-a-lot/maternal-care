ba144c244c8b80f98bfd7e7d733d6ee88b65811a
Reymond Pena <reymond.pena.dev@gmail.com>
2024-09-17 18:23:30 +0800

    modify residence view model to implement addresses

--- app/src/main/java/org/maternalcare/modules/main/residence/viewmodel/ResidenceViewModel.kt
+++ app/src/main/java/org/maternalcare/modules/main/residence/viewmodel/ResidenceViewModel.kt
@@ -2,15 +2,30 @@ package org.maternalcare.modules.main.residence.viewmodel
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import org.maternalcare.modules.main.user.model.dto.AddressDto
 import org.maternalcare.modules.main.user.model.dto.UserDto
+import org.maternalcare.modules.main.user.service.AddressService
 import org.maternalcare.modules.main.user.service.UserService
 import org.mongodb.kbson.ObjectId
 import javax.inject.Inject
 
 @HiltViewModel
 class ResidenceViewModel @Inject constructor(
-    private val userService: UserService
+    private val userService: UserService,
+    private val addressService: AddressService
 ) : ViewModel() {
+    fun fetchAddresses(): List<AddressDto> {
+        return addressService.fetch()
+    }
+
+    fun fetchOneAddress(addressId: ObjectId): AddressDto {
+        return addressService.fetchOne(addressId)
+    }
+
+    suspend fun upsertAddress(addressDto: AddressDto): Result<AddressDto> {
+        return addressService.upsert(addressDto)
+    }
+
     fun fetchUsers(userId: ObjectId): List<UserDto> {
         return userService.fetch(isResidence = true, userId = userId)
     }
