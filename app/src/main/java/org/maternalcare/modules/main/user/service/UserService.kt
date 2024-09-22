9276be42ab366380fb26a1ae5a5bbfa38011ed53
Reymond Pena <reymond.pena.dev@gmail.com>
2024-09-22 12:58:54 +0800

    add fetch checkup details and get group checkup dates function in user service class

--- app/src/main/java/org/maternalcare/modules/main/user/service/UserService.kt
+++ app/src/main/java/org/maternalcare/modules/main/user/service/UserService.kt
@@ -3,13 +3,13 @@ package org.maternalcare.modules.main.user.service
 import io.realm.kotlin.Realm
 import io.realm.kotlin.UpdatePolicy
 import io.realm.kotlin.ext.query
+import io.realm.kotlin.query.Sort
 import org.maternalcare.modules.main.user.model.dto.UserCheckupDto
 import org.maternalcare.modules.main.user.model.dto.UserDto
 import org.maternalcare.modules.main.user.model.entity.User
 import org.maternalcare.modules.main.user.model.entity.UserCheckup
 import org.maternalcare.modules.main.user.model.mapper.toDTO
 import org.maternalcare.modules.main.user.model.mapper.toEntity
-import org.mongodb.kbson.BsonObjectId.Companion.invoke
 import org.mongodb.kbson.ObjectId
 import javax.inject.Inject
 
@@ -97,5 +97,23 @@ class UserService @Inject constructor(private val realm: Realm) {
         } catch (error: Exception) {
             Result.failure(error)
         }
+    }
+
+    fun fetchCheckUpDetails(userId: String): UserCheckupDto? {
+        return realm.query<UserCheckup>(
+            "userId == $0",
+            userId
+        ).sort("scheduleOfNextCheckUp", Sort.DESCENDING)
+            .find()
+            .firstOrNull()
+            ?.run { toDTO() }
+    }
+
+    fun getGroupOfCheckupDates(): List<UserCheckupDto> {
+        return realm.query<UserCheckup>()
+            .distinct("userId")
+            .sort("scheduleOfNextCheckUp", Sort.DESCENDING)
+            .find()
+            .map { it.toDTO() }
     }
 }
