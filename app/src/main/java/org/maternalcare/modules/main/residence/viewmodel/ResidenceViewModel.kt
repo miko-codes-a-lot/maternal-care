b07e25d9f1ad49b74eea8cbd4aa91ffdce49ee76
Miko Chu <miko.codecrafters@gmail.com>
2024-09-23 21:46:19 +0800

    add fetch users by checkup in residence view model & fetch users nullable address

--- app/src/main/java/org/maternalcare/modules/main/residence/viewmodel/ResidenceViewModel.kt
+++ app/src/main/java/org/maternalcare/modules/main/residence/viewmodel/ResidenceViewModel.kt
@@ -26,8 +26,15 @@ class ResidenceViewModel @Inject constructor(
         return addressService.upsert(addressDto)
     }
 
-    fun fetchUsers(userId: ObjectId, isSuperAdmin: Boolean = false, addressName: String): List<UserDto> {
+    fun fetchUsers(userId: ObjectId, isSuperAdmin: Boolean = false, addressName: String?): List<UserDto> {
         val id = if (!isSuperAdmin) userId else null
         return userService.fetch(isResidence = true, userId = id, addressName = addressName)
+    }
+
+    fun fetchUsersByCheckup(
+        userId: ObjectId,
+        dateOfCheckup: String
+    ): List<UserDto> {
+        return userService.fetchByCheckup(userId = userId, dateOfCheckup = dateOfCheckup)
     }
 }
