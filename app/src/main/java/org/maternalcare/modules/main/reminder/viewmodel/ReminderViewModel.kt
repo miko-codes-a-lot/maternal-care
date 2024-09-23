97805cf788bab68ed877b24bc3f79d9c783d166b
Reymond Pena <reymond.pena.dev@gmail.com>
2024-09-23 20:39:31 +0800

    add reminder view model

---
+++ app/src/main/java/org/maternalcare/modules/main/reminder/viewmodel/ReminderViewModel.kt
@@ -0,0 +1,17 @@
+package org.maternalcare.modules.main.reminder.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.maternalcare.modules.main.user.model.dto.UserCheckupDto
+import org.maternalcare.modules.main.user.service.UserService
+import org.maternalcare.shared.ext.toObjectId
+import javax.inject.Inject
+
+@HiltViewModel
+class ReminderViewModel @Inject constructor(
+    private val userService: UserService,
+) : ViewModel() {
+    fun getGroupOfCheckupDate(adminId: String): List<UserCheckupDto> {
+        return userService.getGroupOfCheckupDates(adminId = adminId.toObjectId())
+    }
+}
