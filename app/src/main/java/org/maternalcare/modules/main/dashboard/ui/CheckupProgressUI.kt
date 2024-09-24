1bf6469215e3c673865ee03e3ae2487535f84fc4
Reymond Pena <reymond.pena.dev@gmail.com>
2024-09-24 11:58:19 +0800

    add complete and in complete value in checkup progress ui

--- app/src/main/java/org/maternalcare/modules/main/dashboard/ui/CheckupProgressUI.kt
+++ app/src/main/java/org/maternalcare/modules/main/dashboard/ui/CheckupProgressUI.kt
@@ -1,5 +1,6 @@
 package org.maternalcare.modules.main.dashboard.ui
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,21 +20,29 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import org.maternalcare.modules.main.residence.ui.ListAddress
+import org.maternalcare.modules.main.user.viewmodel.UserViewModel
 
-@Preview
-@Composable
-fun CheckupProgressUIPreview() {
-    CheckupProgressUI(rememberNavController())
-}
+//@Preview
+//@Composable
+//fun CheckupProgressUIPreview() {
+//    CheckupProgressUI(rememberNavController(), true, UserViewModel)
+//}
 
+@SuppressLint("DefaultLocale")
 @Composable
-fun CheckupProgressUI(navController: NavController) {
+fun CheckupProgressUI(
+    navController: NavController,
+    isComplete: Boolean,
+    userViewModel: UserViewModel = hiltViewModel()
+) {
+    val addressCheckupPercentages = userViewModel.getCompleteCheckupPercentages()
+    val overallCompletedPercentage = addressCheckupPercentages["Overall Completed Address Percentage"] ?: 0.0
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -45,14 +54,15 @@ fun CheckupProgressUI(navController: NavController) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             //Sample value
-            val completePercentage = "80%"
-            val incompletePercentage = "20%"
+            val completePercentage = "${String.format("%.2f", overallCompletedPercentage)}%"
+            val incompletePercentage = "${String.format("%.2f", 100 - overallCompletedPercentage)}%"
 
             Spacer(modifier = Modifier.padding(top = 50.dp))
 
             AverageStatusContainer(
                 completePercentage = completePercentage,
-                incompletePercentage = incompletePercentage
+                incompletePercentage = incompletePercentage,
+                isComplete = isComplete
             )
 
             Spacer(modifier = Modifier.height(30.dp))
@@ -63,22 +73,27 @@ fun CheckupProgressUI(navController: NavController) {
 }
 
 @Composable
-fun AverageStatusContainer(completePercentage: String, incompletePercentage: String) {
+fun AverageStatusContainer(completePercentage: String, incompletePercentage: String, isComplete: Boolean) {
     ElevatedCard(
         elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
         colors = CardDefaults.elevatedCardColors(
             containerColor = Color(0xFF6650a4),
             contentColor = Color.White
         ),
-        modifier = Modifier.size(width = 290.dp, height = 110.dp)
+        modifier = Modifier.size(width = 265.dp, height = 105.dp)
     ) {
         Box(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                AverageStats(label = "Complete  ", value = completePercentage,)
+            val result = if (isComplete) {
+                AverageStats(label = "Complete", value = completePercentage)
+            } else {
                 AverageStats(label = "Incomplete", value = incompletePercentage)
+            }
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                result
             }
         }
     }
@@ -87,21 +102,20 @@ fun AverageStatusContainer(completePercentage: String, incompletePercentage: Str
 @Composable
 fun AverageStats(label: String, value: String) {
     Row {
-
         Text(
             text = label,
             modifier = Modifier.padding(10.dp),
-            fontSize = 17.sp,
+            fontSize = 19.sp,
             textAlign = TextAlign.Center,
             fontFamily = FontFamily.Monospace,
         )
 
-        Spacer(modifier = Modifier.width(75.dp))
+        Spacer(modifier = Modifier.width(50.dp))
 
         Text(
             text = value,
             modifier = Modifier.padding(10.dp),
-            fontSize = 17.sp,
+            fontSize = 18.sp,
             textAlign = TextAlign.Center,
             fontFamily = FontFamily.Monospace,
         )
