9d53210edf7cca7ad269b0a1dbc55793f877ccd4
Reymond Pena <reymond.pena.dev@gmail.com>
2024-09-28 20:38:52 +0800

    add implementation libs.itextpdf in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.coil.compose)
+    implementation (libs.itextpdf)
 }
 
 kapt {
