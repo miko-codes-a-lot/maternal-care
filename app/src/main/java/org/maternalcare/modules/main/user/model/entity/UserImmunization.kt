5c1344c1ecea76105bbed4b00c0a30e912eceb75
Reymond Pena <reymond.pena.dev@gmail.com>
2024-10-03 18:49:16 +0800

    add user Immunization Entity

---
+++ app/src/main/java/org/maternalcare/modules/main/user/model/entity/UserImmunization.kt
@@ -0,0 +1,29 @@
+package org.maternalcare.modules.main.user.model.entity
+
+import io.realm.kotlin.types.RealmInstant
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.BsonObjectId
+import org.mongodb.kbson.ObjectId
+
+class UserImmunization : RealmObject {
+    @PrimaryKey
+    var _id: ObjectId = BsonObjectId()
+    var userId: String = ""
+    var firstDoseGiven: RealmInstant = RealmInstant.now()
+    var firstDoseReturn: RealmInstant = RealmInstant.now()
+    var secondDoseGiven: RealmInstant = RealmInstant.now()
+    var secondDoseReturn: RealmInstant = RealmInstant.now()
+    var thirdDoseGiven: RealmInstant = RealmInstant.now()
+    var thirdDoseReturn: RealmInstant = RealmInstant.now()
+    var fourthDoseGiven: RealmInstant = RealmInstant.now()
+    var fourthDoseReturn: RealmInstant = RealmInstant.now()
+    var fifthDoseGiven: RealmInstant = RealmInstant.now()
+    var fifthDoseReturn: RealmInstant = RealmInstant.now()
+    var createdById: ObjectId? = null
+    var createdAt: RealmInstant = RealmInstant.now()
+    var lastUpdatedById: ObjectId? = null
+    var lastUpdatedAt: RealmInstant = RealmInstant.now()
+    var deletedById: ObjectId? = null
+    var deletedAt: RealmInstant? = null
+}
