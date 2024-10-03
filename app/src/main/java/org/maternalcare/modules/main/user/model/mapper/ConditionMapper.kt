9e0061ab535437bb67877b01053c5d423017380d
Reymond Pena <reymond.pena.dev@gmail.com>
2024-10-03 18:46:55 +0800

    add Condition Mapper for user condition dto

---
+++ app/src/main/java/org/maternalcare/modules/main/user/model/mapper/ConditionMapper.kt
@@ -0,0 +1,79 @@
+package org.maternalcare.modules.main.user.model.mapper
+
+import io.realm.kotlin.types.RealmInstant
+import org.maternalcare.shared.ext.toInstantStringNullable
+import org.maternalcare.shared.ext.toObjectId
+import org.maternalcare.modules.main.user.model.dto.UserConditionDto
+import org.maternalcare.modules.main.user.model.entity.UserCondition
+import org.maternalcare.shared.ext.toRealmInstantNullable
+
+fun UserCondition.toDTO(): UserConditionDto{
+    return UserConditionDto(
+        id = _id.toHexString(),
+        userId = userId,
+        tuberculosisPersonal = tuberculosisPersonal,
+        tuberculosisFamily = tuberculosisFamily,
+        heartDiseasesPersonal = heartDiseasesPersonal,
+        heartDiseasesFamily = heartDiseasesFamily,
+        diabetesPersonal = diabetesPersonal,
+        diabetesFamily = diabetesFamily,
+        hypertensionPersonal = hypertensionPersonal,
+        hypertensionFamily = hypertensionFamily,
+        branchialAsthmaPersonal = branchialAsthmaPersonal,
+        branchialAsthmaFamily = branchialAsthmaFamily,
+        urinaryTractInfectionPersonal = urinaryTractInfectionPersonal,
+        urinaryTractInfectionFamily = urinaryTractInfectionFamily,
+        parasitismPersonal = parasitismPersonal,
+        parasitismFamily = parasitismFamily,
+        goitersPersonal = goitersPersonal,
+        goitersFamily = goitersFamily,
+        anemiaPersonal = anemiaPersonal,
+        anemiaFamily = anemiaFamily,
+        isUnderWeight = isUnderWeight,
+        isOverWeight = isOverWeight,
+        genitalTractInfection = genitalTractInfection,
+        otherInfectionsDiseases = otherInfectionsDiseases,
+        createdById = createdById?.toHexString(),
+        createdAt = createdAt.toInstantStringNullable(),
+        lastUpdatedById = lastUpdatedById?.toHexString(),
+        lastUpdatedAt = lastUpdatedAt.toInstantStringNullable(),
+        deletedById = deletedById?.toHexString(),
+        deletedAt = deletedAt?.run { this.toInstantStringNullable() },
+    )
+}
+
+fun UserConditionDto.toEntity(): UserCondition {
+    val conditionDto = this
+    return UserCondition().apply {
+        _id = conditionDto.id.toObjectId()
+        userId = conditionDto.userId
+        tuberculosisPersonal = conditionDto.tuberculosisPersonal
+        tuberculosisFamily = conditionDto.tuberculosisFamily
+        heartDiseasesPersonal = conditionDto.heartDiseasesPersonal
+        heartDiseasesFamily = conditionDto.heartDiseasesFamily
+        diabetesPersonal = conditionDto.diabetesPersonal
+        diabetesFamily = conditionDto.diabetesFamily
+        hypertensionPersonal = conditionDto.hypertensionPersonal
+        hypertensionFamily = conditionDto.hypertensionFamily
+        branchialAsthmaPersonal = conditionDto.branchialAsthmaPersonal
+        branchialAsthmaFamily = conditionDto.branchialAsthmaFamily
+        urinaryTractInfectionPersonal = conditionDto.urinaryTractInfectionPersonal
+        urinaryTractInfectionFamily = conditionDto.urinaryTractInfectionFamily
+        parasitismPersonal = conditionDto.parasitismPersonal
+        parasitismFamily = conditionDto.parasitismFamily
+        goitersPersonal = conditionDto.goitersPersonal
+        goitersFamily = conditionDto.goitersFamily
+        anemiaPersonal = conditionDto.anemiaPersonal
+        anemiaFamily = conditionDto.anemiaFamily
+        isUnderWeight = conditionDto.isUnderWeight
+        isOverWeight = conditionDto.isOverWeight
+        genitalTractInfection = conditionDto.genitalTractInfection
+        otherInfectionsDiseases = conditionDto.otherInfectionsDiseases
+        createdById = conditionDto.createdById?.toObjectId()
+        createdAt = conditionDto.createdAt.toRealmInstantNullable() ?: RealmInstant.now()
+        lastUpdatedById = conditionDto.lastUpdatedById?.toObjectId()
+        lastUpdatedAt = conditionDto.createdAt.toRealmInstantNullable() ?: RealmInstant.now()
+        deletedById = conditionDto.deletedById?.toObjectId()
+        deletedAt = conditionDto.deletedAt.toRealmInstantNullable()
+    }
+}
