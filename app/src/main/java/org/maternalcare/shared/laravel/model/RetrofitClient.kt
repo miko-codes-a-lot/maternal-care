07bb02cd601edde58a11508a1d3f8214d100db0a
Reymond Pena <reymond.pena.dev@gmail.com>
2024-10-25 16:32:00 +0800

    add object container for retrofitclient that contain the url of ngrok

---
+++ app/src/main/java/org/maternalcare/shared/laravel/model/RetrofitClient.kt
@@ -0,0 +1,24 @@
+package org.maternalcare.shared.laravel.model // This Laravel Container Folder Name
+
+import okhttp3.OkHttpClient
+import org.maternalcare.shared.laravel.ApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitClient {
+    private const val BASE_URL = "https://c803-124-107-252-217.ngrok-free.app/api/"
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    val apiService: ApiService = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+}
