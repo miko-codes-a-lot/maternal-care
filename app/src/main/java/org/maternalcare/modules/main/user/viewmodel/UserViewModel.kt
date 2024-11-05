67c7a8a97fc0c36325686fa6d3a88c9c500f31c7
Reymond Pena <reymond.pena.dev@gmail.com>
2024-11-05 17:23:31 +0800

    delete some import that not necessary used

--- app/src/main/java/org/maternalcare/modules/main/user/viewmodel/UserViewModel.kt
+++ app/src/main/java/org/maternalcare/modules/main/user/viewmodel/UserViewModel.kt
@@ -3,6 +3,9 @@ package org.maternalcare.modules.main.user.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import org.maternalcare.modules.main.user.model.dto.UserBirthRecordDto
 import org.maternalcare.modules.main.user.model.dto.UserCheckupDto
