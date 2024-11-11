2f2adb5245e7ad753c69ee5f840d7119e51da1ad
Reymond Pena <reymond.pena.dev@gmail.com>
2024-11-11 21:47:16 +0800

    add another function of fetch latest message and update message read status in message service

--- app/src/main/java/org/maternalcare/modules/main/message/service/MessageService.kt
+++ app/src/main/java/org/maternalcare/modules/main/message/service/MessageService.kt
@@ -6,6 +6,7 @@ import io.realm.kotlin.ext.query
 import io.realm.kotlin.notifications.ResultsChange
 import io.realm.kotlin.query.Sort
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import org.maternalcare.modules.main.message.mapper.toDTO
 import org.maternalcare.modules.main.message.mapper.toEntity
 import org.maternalcare.modules.main.message.model.dto.MessageDto
@@ -35,11 +36,34 @@ class MessageService @Inject constructor(private val realm: Realm) {
     suspend fun send(messageDto: MessageDto): Result<MessageDto> {
         return try {
             realm.write {
+                messageDto.isRead = false
                 val message = copyToRealm(messageDto.toEntity(), updatePolicy = UpdatePolicy.ALL)
                 Result.success(message.toDTO())
             }
         } catch (error: Exception) {
             Result.failure(error)
+        }
+    }
+
+    fun fetchLatestMessage(senderId: ObjectId, receiverId: ObjectId): Flow<MessageDto?> {
+        val query = realm.query<Message>(
+            "senderId == $0 AND receiverId == $1 OR senderId == $1 AND receiverId == $0",
+            senderId, receiverId
+        ).sort("date", Sort.DESCENDING)
+
+        return query.asFlow().map { results ->
+            results.list.firstOrNull()?.toDTO()
+        }
+    }
+
+    suspend fun updateMessageReadStatus(messageDto: MessageDto) {
+        realm.write {
+            val messageId = ObjectId(messageDto.id ?: throw IllegalArgumentException("Invalid message ID"))
+            val message = query<Message>("_id == $0", messageId).first().find()
+            if (message != null) {
+                message.isRead = true
+                copyToRealm(message, updatePolicy = UpdatePolicy.ALL)
+            }
         }
     }
 }
