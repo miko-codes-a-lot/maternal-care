994b6a5c49f166126e03983e47b5bff13bea1fd6
Reymond Pena <reymond.pena.dev@gmail.com>
2024-11-11 13:39:28 +0800

    add another data variable in address entity and address dto

--- app/src/main/java/org/maternalcare/modules/main/user/model/dto/AddressDto.kt
+++ app/src/main/java/org/maternalcare/modules/main/user/model/dto/AddressDto.kt
@@ -3,5 +3,7 @@ package org.maternalcare.modules.main.user.model.dto
 data class AddressDto(
     var id: String?,
     var name: String,
-    var code: String
+    var code: String,
+    var latitude: Double = 0.0,
+    var longitude: Double = 0.0,
 )
