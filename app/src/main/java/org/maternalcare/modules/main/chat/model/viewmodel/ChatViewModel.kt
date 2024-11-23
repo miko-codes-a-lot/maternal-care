637d771d3b0e99e525b10211cdf20767d2350070
Miko Chu <miko.codecrafters@gmail.com>
2024-11-23 23:54:21 +0800

    expose get direct messages & send message in chat view model

--- app/src/main/java/org/maternalcare/modules/main/chat/model/viewmodel/ChatViewModel.kt
+++ app/src/main/java/org/maternalcare/modules/main/chat/model/viewmodel/ChatViewModel.kt
@@ -3,8 +3,11 @@ package org.maternalcare.modules.main.chat.model.viewmodel
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import org.maternalcare.modules.main.chat.model.dto.ChatDto
+import org.maternalcare.modules.main.chat.model.dto.MessageDto
 import org.maternalcare.modules.main.chat.model.dto.UserChatDto
 import org.maternalcare.modules.main.chat.service.ChatService
+import org.maternalcare.modules.main.user.model.dto.UserDto
 import org.mongodb.kbson.ObjectId
 import javax.inject.Inject
 
@@ -14,5 +17,17 @@ class ChatViewModel @Inject constructor(
 ): ViewModel() {
     fun fetchUsers(userId: ObjectId): Flow<List<UserChatDto>> {
         return chatService.fetchUsers(userId)
+    }
+
+    suspend fun findOneChatOrCreate(sender: UserDto, receiver: UserDto): Result<ChatDto> {
+        return chatService.findOneChatOrCreate(sender, receiver)
+    }
+
+    fun fetchDirectMessages(sender: UserDto, receiver: UserDto): Flow<List<MessageDto>> {
+        return chatService.fetchDirectMessages(sender, receiver)
+    }
+
+    suspend fun sendMessage(sender: UserDto, receiver: UserDto, content: String): Result<MessageDto> {
+        return chatService.message(sender, receiver, content)
     }
 }
