287fb920c991bb10aaa46089e21904ca697f2f49
Miko Chu <miko.codecrafters@gmail.com>
2024-11-23 19:13:49 +0800

    modify realm instant extension to instant kotlin date time

--- app/src/main/java/org/maternalcare/shared/ext/RealmInstantExt.kt
+++ app/src/main/java/org/maternalcare/shared/ext/RealmInstantExt.kt
@@ -2,10 +2,15 @@ package org.maternalcare.shared.ext
 
 import io.realm.kotlin.types.RealmInstant
 import java.time.Instant
+import kotlinx.datetime.Instant as InstantK
 
 fun RealmInstant.toInstantString(): String {
     val instant = Instant.ofEpochSecond(epochSeconds, nanosecondsOfSecond.toLong())
     return instant.toString()
+}
+
+fun RealmInstant.toInstantK(): InstantK {
+    return InstantK.fromEpochSeconds(epochSeconds, nanosecondsOfSecond.toLong())
 }
 
 fun RealmInstant?.toInstantStringNullable(): String? {
