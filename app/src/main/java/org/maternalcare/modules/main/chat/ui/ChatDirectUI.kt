122cfa9f4ee83505d63ddde521da955ab3c8276b
Reymond Pena <reymond.pena.dev@gmail.com>
2024-11-24 20:55:11 +0800

    modify chat direct ui and chat lobby ui

--- app/src/main/java/org/maternalcare/modules/main/chat/ui/ChatDirectUI.kt
+++ app/src/main/java/org/maternalcare/modules/main/chat/ui/ChatDirectUI.kt
@@ -1,24 +1,33 @@
 package org.maternalcare.modules.main.chat.ui
 
+import android.icu.text.SimpleDateFormat
+import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.Button
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,13 +36,20 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
+import org.maternalcare.R
 import org.maternalcare.modules.main.chat.model.dto.MessageDto
+import java.util.Locale
 
 
 @Preview(showSystemUi = true)
@@ -66,7 +82,6 @@ fun ChatDirectPreview() {
     )
 }
 
-
 @Composable
 fun ChatDirectUI(
     messages: List<MessageDto>,
@@ -77,11 +92,19 @@ fun ChatDirectUI(
     val isSendingMessage = remember { mutableStateOf(false) }
     val scope = rememberCoroutineScope()
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(16.dp),
+    ) {
         LazyColumn(
-            modifier = Modifier.weight(1f),
+            modifier = Modifier
+                .padding(top = 40.dp)
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .weight(1f),
             reverseLayout = true,
-            contentPadding = PaddingValues(8.dp)
         ) {
             items(items = messages) { message ->
                 MessageBubble(
@@ -90,75 +113,113 @@ fun ChatDirectUI(
                 )
             }
         }
-        HorizontalDivider()
         Row(
             modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .background(Color.White, RoundedCornerShape(8.dp))
+                .border(1.dp, Color(0xFF6650a4), RoundedCornerShape(8.dp))
+                .padding(8.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            BasicTextField(
+            TextField(
                 value = messageContent,
                 onValueChange = { messageContent = it },
+                placeholder = { Text("Type your message...",color = Color.Black)},
                 modifier = Modifier
-                    .weight(1f)
-                    .padding(8.dp)
-                    .background(Color.LightGray, RoundedCornerShape(16.dp))
-                    .padding(8.dp)
+                    .weight(0.7f)
+                    .padding(end = 2.dp)
+                    .verticalScroll(rememberScrollState()),
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedContainerColor = Color.White,
+                    focusedTextColor = Color.Black,
+                    unfocusedTextColor = Color.Black,
+                    focusedBorderColor = Color(0xFF6650a4),
+                    unfocusedBorderColor = Color(0xFF6650a4)
+                ),
+                maxLines = 2
             )
-            Spacer(modifier = Modifier.width(8.dp))
-            Button(
-                enabled = !isSendingMessage.value,
-                onClick = {
-                    if (isSendingMessage.value) return@Button
-
-                    isSendingMessage.value = true
-                    if (messageContent.text.isNotBlank()) {
-                        scope.launch {
-                            onSendMessage(messageContent.text)
-                            messageContent = TextFieldValue("")
-                            isSendingMessage.value = false
+            Icon(
+                painter = painterResource(id = R.drawable.icon_send),
+                contentDescription = "Send",
+                modifier = Modifier
+                    .size(25.dp)
+                    .clickable {
+                        if (isSendingMessage.value) return@clickable
+                        isSendingMessage.value = true
+                        if (messageContent.text.isNotBlank()) {
+                            scope.launch {
+                                onSendMessage(messageContent.text)
+                                messageContent = TextFieldValue("")
+                                isSendingMessage.value = false
+                            }
                         }
-                    }
-                }
-            ) {
-                Text("Send")
-            }
+                    },
+                tint = Color(0xFF6650a4)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
         }
+        Spacer(modifier = Modifier.height(45.dp))
     }
 }
 
 @Composable
 fun MessageBubble(message: MessageDto, isSentByCurrentUser: Boolean) {
     val alignment = if (isSentByCurrentUser) Alignment.CenterEnd else Alignment.CenterStart
-    val backgroundColor = if (isSentByCurrentUser) Color(0xFFDCF8C6) else Color.White
-    val textColor = if (isSentByCurrentUser) Color.Black else Color.DarkGray
-    val padding = if (isSentByCurrentUser) PaddingValues(start = 64.dp, end = 8.dp)
-    else PaddingValues(start = 8.dp, end = 64.dp)
+    if (isSentByCurrentUser) {
+        PaddingValues(start = 64.dp, end = 8.dp)
+    }else {
+        PaddingValues(start = 8.dp, end = 64.dp)
+    }
+    val (containerColor, contentColor) =
+        if (isSentByCurrentUser) {
+            Color(0xFF6650a4) to Color(0xFFFFFFFF)
+        } else {
+            Color(0xFFcccccf) to Color.Black
+        }
 
+    val displayFormat = remember { SimpleDateFormat("MMM-dd-yyyy", Locale.getDefault()) }
+    val formattedDate = try {
+        val parsedDate = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault()).parse(message.createdAt.toString())
+        parsedDate?.let { displayFormat.format(it) } ?: "Invalid Date"
+    } catch (e: Exception) {
+        "Invalid Date"
+    }
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(padding),
+            .padding(0.dp),
         contentAlignment = alignment
     ) {
-        Column(
+        Surface(
             modifier = Modifier
-                .background(backgroundColor, RoundedCornerShape(8.dp))
-                .padding(8.dp)
+                .fillMaxWidth(0.7f)
+                .padding(top = 10.dp, bottom = 10.dp),
+            shape = RoundedCornerShape(12.dp),
+            color = containerColor,
+            contentColor = contentColor
         ) {
-            Text(
-                text = message.content,
-                color = textColor,
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = message.createdAt.toString(), // Format as needed
-                color = Color.Gray,
-                style = MaterialTheme.typography.labelSmall
-            )
+            Column(modifier = Modifier
+                .padding(12.dp)
+            ) {
+                Text(
+                    text = message.content,
+                    maxLines = Int.MAX_VALUE,
+                    overflow = TextOverflow.Visible,
+                    fontFamily = FontFamily.SansSerif,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = formattedDate,
+                    fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                    textAlign = TextAlign.End,
+                    fontFamily = FontFamily.SansSerif,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .alpha(0.38f),
+                    style = MaterialTheme.typography.labelSmall
+                )
+            }
         }
     }
 }
-
--- app/src/main/java/org/maternalcare/modules/main/chat/ui/ChatLobbyUI.kt
+++ app/src/main/java/org/maternalcare/modules/main/chat/ui/ChatLobbyUI.kt
@@ -1,41 +1,71 @@
 package org.maternalcare.modules.main.chat.ui
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberAsyncImagePainter
+import kotlinx.coroutines.delay
 import kotlinx.datetime.Clock
+import org.maternalcare.modules.main.MainNav
 import org.maternalcare.modules.main.chat.model.dto.ChatDto
 import org.maternalcare.modules.main.chat.model.dto.UserChatDto
+import org.maternalcare.modules.main.residence.ui.SearchAddressIcon
 import org.maternalcare.modules.main.user.model.dto.UserDto
+import org.maternalcare.modules.main.user.ui.resizeBitmap
 
 @Preview(showSystemUi = true)
 @Composable
 fun ChatLobbyUIPreview() {
     ChatLobbyUI(
+        rememberNavController(),
         listOf(
             UserChatDto(
                 UserDto(
@@ -76,58 +106,215 @@ fun ChatLobbyUIPreview() {
 }
 
 @Composable
-fun ChatLobbyUI(data: List<UserChatDto>) {
-    LazyColumn {
-        items(items = data) { userChat ->
-            ChatCard(userChat)
+fun ChatLobbyUI(navController: NavController, data: List<UserChatDto>) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var searchQuery by remember { mutableStateOf("") }
+        var debouncedQuery by remember { mutableStateOf("") }
+
+        LaunchedEffect(searchQuery) {
+            delay(500L)
+            debouncedQuery = searchQuery
+        }
+
+        val filteredData = remember(debouncedQuery, data) {
+            if (debouncedQuery.isEmpty()) {
+                data
+            } else {
+                data.filter { userChat ->
+                    val fullName = "${userChat.userDto.firstName} ${userChat.userDto.middleName.orEmpty()} ${userChat.userDto.lastName}"
+                    fullName.contains(debouncedQuery, ignoreCase = true)
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        SearchMessageIcon(
+            searchQuery = searchQuery,
+            onSearchQueryChanged = { searchQuery = it },
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+        LazyColumn {
+            items(items = filteredData) { userChat ->
+                ChatCard(navController = navController, userChat = userChat, searchQuery = debouncedQuery)
+            }
         }
     }
 }
 
 @Composable
-fun ChatCard(userChat: UserChatDto) {
+fun ChatCard(userChat: UserChatDto, navController: NavController, searchQuery: String = "") {
     val user = userChat.userDto
     val chat = userChat.chatDto
 
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
-            .clickable { /* Handle click event */ },
+            .padding(top = 5.dp, bottom = 5.dp)
+            .clickable {
+                navController.navigate(MainNav.ChatDirect(userId = userChat.userDto.id!!))
+            },
+        colors = CardDefaults.cardColors(
+            contentColor = Color.Black,
+            containerColor = Color.White,
+        )
     ) {
         Row(
             modifier = Modifier.padding(8.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            // User Avatar
-            Image(
-                painter = rememberImagePainter(data = "https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3467.jpg"),
-                contentDescription = "${user.firstName} ${user.lastName}'s avatar",
-                modifier = Modifier
-                    .size(48.dp)
-                    .clip(CircleShape)
-                    .background(Color.Gray)
-            )
-
+            CardImageContainer(userChat.userDto.imageBase64)
             Spacer(modifier = Modifier.width(8.dp))
 
-            // User Name and Last Message
             Column {
                 Text(
                     text = "${user.firstName} ${user.middleName} ${user.lastName}",
-                    style = MaterialTheme.typography.labelMedium,
+                    fontSize = 15.sp,
+                    fontFamily = FontFamily.SansSerif,
                     fontWeight = FontWeight.Bold
                 )
-                Spacer(modifier = Modifier.height(4.dp))
                 Text(
                     text = chat.lastMessage,
-                    style = MaterialTheme.typography.bodyMedium,
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily.SansSerif,
                     fontWeight = if (chat.isRead) FontWeight.Normal else FontWeight.Bold,
                     color = if (chat.isRead) Color.Gray else Color.Black,
                     maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .padding(bottom = 3.dp)
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            if (!chat.isRead) {
+                Box(
+                    modifier = Modifier
+                        .background(
+                            color = Color.Red,
+                            shape = CircleShape
+                        )
+                        .size(13.dp)
+                        .border(
+                            width = 1.dp,
+                            color = Color.Red,
+                            shape = CircleShape
+                        ),
                 )
             }
         }
     }
+}
+@Composable
+fun CardImageContainer(imageBase64: String? = null) {
+    Box(
+        Modifier.height(55.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(51.dp)
+                .clip(CircleShape)
+                .background(Color(0xFF6650a4))
+                .border(1.dp, Color(0xFF6650a4), CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+            if (imageBase64 != null && imageBase64.isNotBlank()) {
+                val resizedBitmap = remember(imageBase64) {
+                    decodeBase64ToBitmap(imageBase64)?.let { bitmap ->
+                        resizeBitmap(bitmap, maxWidth = 500, maxHeight = 500)
+                    }
+                }
+
+                if (resizedBitmap != null) {
+                    Image(
+                        bitmap = resizedBitmap.asImageBitmap(),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(51.dp)
+                            .clip(CircleShape)
+                    )
+                } else {
+                    PlaceholderImage()
+                }
+            } else {
+                PlaceholderImage()
+            }
+        }
+    }
+}
+
+@Composable
+fun PlaceholderImage() {
+    Image(
+        painter = rememberAsyncImagePainter(model = "https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3467.jpg"),
+        contentDescription = "User's avatar",
+        modifier = Modifier
+            .size(51.dp)
+            .clip(CircleShape)
+            .background(Color(0xFF6650a4)),
+    )
+}
+
+fun decodeBase64ToBitmap(base64Str: String): Bitmap? {
+    return try {
+        val decodedBytes = Base64.decode(base64Str, Base64.DEFAULT)
+        BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+    } catch (e: IllegalArgumentException) {
+        e.printStackTrace()
+        null
+    }
+}
+
+@Composable
+fun SearchMessageIcon(
+    searchQuery: String,
+    onSearchQueryChanged: (String) -> Unit,
+) {
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = searchQuery,
+        onValueChange = { onSearchQueryChanged(it) },
+        leadingIcon = {
+            IconButton(onClick = { } ) {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = "Search Icon",
+                    tint = Color.Black,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        },
+        trailingIcon = {
+            if (searchQuery.isNotEmpty()) {
+                IconButton(onClick = { onSearchQueryChanged("") }) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = "Clear Search",
+                        tint = Color.Black,
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            }
+        },
+        colors = OutlinedTextFieldDefaults.colors(
+            unfocusedBorderColor = Color.Black,
+            focusedTextColor = Color.Black,
+            unfocusedTextColor = Color.Black,
+            focusedBorderColor = Color.Black,
+            disabledBorderColor = Color.Gray,
+            errorBorderColor = Color.Red,
+            cursorColor = Color.Black
+        ),
+        placeholder = {
+            Text(text = "Search message...", color = Color.Black)
+        }
+    )
 }
