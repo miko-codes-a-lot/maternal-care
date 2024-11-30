faf2c849a2137e4f35cae7bdfee748f5c4c8ff91
Reymond Pena <reymond.pena.dev@gmail.com>
2024-11-30 19:29:01 +0800

    increase compile & target sdk to 35 from 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,12 +12,12 @@ plugins {
 
 android {
     namespace = "org.maternalcare"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "org.maternalcare"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
