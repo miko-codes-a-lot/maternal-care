167a4baa80a1ebf10b0371acd02b0c819dd2bbd1
Reymond Pena <reymond.pena.dev@gmail.com>
2024-12-08 00:34:36 +0800

    add new html file and function for bhw manual, midWife manual, pregnant manual

---
+++ app/src/main/java/org/maternalcare/shared/html_viewer/BhwManualViewer.kt
@@ -0,0 +1,23 @@
+package org.maternalcare.shared.html_viewer
+
+import android.webkit.WebView
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun BhwManualViewer(
+    fileName: String
+) {
+    val context = LocalContext.current
+    AndroidView(
+        factory = {
+            WebView(context).apply {
+                loadUrl("file:///android_asset/$fileName")
+            }
+        },
+        modifier = Modifier.fillMaxSize()
+    )
+}
